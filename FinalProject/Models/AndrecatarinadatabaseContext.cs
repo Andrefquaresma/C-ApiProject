726a1dbd3b26600209e8a3d2cf90effabfaaa61b
André Quaresma <andre.quaresma@solera.com>
2022-11-18 16:48:05 +0100

    Full Project C# APi MVC

---
+++ FinalProject/Models/AndrecatarinadatabaseContext.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinalProject.Models;
+
+public partial class AndrecatarinadatabaseContext : DbContext
+{
+
+    public AndrecatarinadatabaseContext(DbContextOptions<AndrecatarinadatabaseContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Claim> Claims { get; set; }
+
+    public virtual DbSet<Owner> Owners { get; set; }
+
+    public virtual DbSet<Vehicle> Vehicles { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-B8F5F91;Database=andrecatarinadatabase;Integrated Security=True;TrustServerCertificate=True");
+
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Claim>(entity =>
+        {
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.Date).HasColumnType("date");
+            entity.Property(e => e.Description)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Status)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.VehicleId).HasColumnName("Vehicle_Id");
+
+            entity.HasOne(d => d.Vehicle).WithMany(p => p.Claims)
+                .HasForeignKey(d => d.VehicleId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Claims_Vehicles");
+        });
+
+        modelBuilder.Entity<Owner>(entity =>
+        {
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.DriverLicense)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.FirstName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.LastName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Vehicle>(entity =>
+        {
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.Brand).HasMaxLength(50);
+            entity.Property(e => e.Color).HasMaxLength(50);
+            entity.Property(e => e.OwnerId).HasColumnName("Owner_Id");
+            entity.Property(e => e.Vin).HasMaxLength(50);
+
+            entity.HasOne(d => d.Owner).WithMany(p => p.Vehicles)
+                .HasForeignKey(d => d.OwnerId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Vehicles_Owners");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
